bbfd607a011e835a8545abc9b178737b114c159f
Bruno Bartoli <bruno_bartoli@hotmail.com>
2022-08-31 23:32:01 -0300

    Primer entrega

---
+++ desafio1.js
@@ -0,0 +1,20 @@
+
+let num = Number(prompt("Ingrese el numero a multiplicar"))
+
+for (let i = 1; i <= 10 ; i++) {
+
+    resul = num * i;
+    alert(`el resultad de ${num} x ${i} es ${resul}`);
+    
+    // if (i === 10){
+    //     let usuario = prompt(" si desea multiplicar otro numero, ingreselo, de lo contario escriba esc")
+    //     if (usuario === "esc"){
+    //         alert("gracias")
+    //         break
+    //     }else{
+    //         let num = Number(prompt("numero"))
+    //         let i = 1
+    //         continue
+    //     }
+    // } 
+}

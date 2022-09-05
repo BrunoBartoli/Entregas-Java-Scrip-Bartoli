c433eaf1fabb680335845fedbf32e65407b5743a
Bruno Bartoli <bruno_bartoli@hotmail.com>
2022-09-04 23:52:33 -0300

    desafio1

--- desafio1.js
+++ desafio1.js
@@ -6,15 +6,15 @@ for (let i = 1; i <= 10 ; i++) {
     resul = num * i;
     alert(`el resultad de ${num} x ${i} es ${resul}`);
     
-    // if (i === 10){
-    //     let usuario = prompt(" si desea multiplicar otro numero, ingreselo, de lo contario escriba esc")
-    //     if (usuario === "esc"){
-    //         alert("gracias")
-    //         break
-    //     }else{
-    //         let num = Number(prompt("numero"))
-    //         let i = 1
-    //         continue
-    //     }
-    // } 
+    if (i === 10){
+        let usuario = prompt(" si desea multiplicar otro numero, ingreselo, de lo contario escriba esc")
+        if (usuario === "esc"){
+            alert("gracias")
+            break
+        }else{
+            num = usuario
+            i = 0
+            continue
+        }
+    } 
 }

f79b4d951fdd91cc9f66fb99303bd00cfb5f2df1
Bruno Bartoli <bruno_bartoli@hotmail.com>
2022-09-06 23:09:50 -0300

    Desafio 1

--- desafio1.js
+++ desafio1.js
@@ -1,17 +1,20 @@
-let edad = Number(prompt("Ingrese una edad"))
-let cont = 1
-let prome = 0
-let resul = 0
-
-for (i = 0; i = 1 ; cont = cont + 1){
-    if (edad <= 1)
-    resul = edad + resul
-    
-    edad = prompt("ingrese otra edad o esc para terminar")
-    if (edad === "esc"){
-        i = 1
-    }
+// CREAMOS LAS FUNCIONES
+function promedio( suma, cont) {
+    prome = suma / cont
+    return alert(`el promedio es ${prome}`)
 }
-prome = resul / cont
-alert(`el promedio es ${prome}`)
+// DECLARAMOS VARIABLES
+let edad = Number(prompt("Ingrese una edad"))
+let cont = 0
+let suma = 0
+// EJECUTAMOS EL PROGRAMA
+while (edad >= 1) {
+    cont++
+    suma = edad + suma
+    edad = Number(prompt("ingrese otra edad o 0 para terminar"))
+}
+
+promedio(suma,cont)
+
+
 
